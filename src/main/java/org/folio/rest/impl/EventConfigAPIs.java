0333ca32fa5e1580bc06e9cc7757b05bd68cb16e
Dmytro Tur <dmytro_tur@epam.com>
2019-04-19 15:06:51 +0300

    Refactor get all implementation

--- src/main/java/org/folio/rest/impl/EventConfigAPIs.java
+++ src/main/java/org/folio/rest/impl/EventConfigAPIs.java
@@ -2,6 +2,7 @@ package org.folio.rest.impl;
 
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Context;
+import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.logging.Logger;
@@ -15,18 +16,19 @@ import org.folio.rest.persist.Criteria.Offset;
 import org.folio.rest.persist.PgUtil;
 import org.folio.rest.persist.PostgresClient;
 import org.folio.rest.persist.cql.CQLWrapper;
+import org.folio.rest.persist.interfaces.Results;
 import org.z3950.zing.cql.cql2pgjson.CQL2PgJSON;
+import org.z3950.zing.cql.cql2pgjson.FieldException;
 
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.List;
 import java.util.Map;
-
-import static io.vertx.core.Future.succeededFuture;
 
 public class EventConfigAPIs implements EventConfig {
 
   private final Logger logger = LoggerFactory.getLogger(EventConfigAPIs.class);
 
+  private static final String INTERNAL_SERVER_ERROR = "Internal server error";
   private static final String EVENT_CONFIGS = "event_configurations";
 
   private final Vertx vertx;
@@ -40,45 +42,49 @@ public class EventConfigAPIs implements EventConfig {
   @Override
   public void getEventConfig(String query, int offset, int limit, String lang, Map<String, String> okapiHeaders,
                              Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
+    buildSqlWrapper(query, offset, limit)
+      .compose(cqlWrapper -> getEventConfigCollection(cqlWrapper, vertx, tenantId))
+      .map(this::mapResultsToConfigCollections)
+      .map(GetEventConfigResponse::respond200WithApplicationJson)
+      .map(Response.class::cast)
+      .otherwise(this::mapException)
+      .setHandler(asyncResultHandler);
+  }
+
+  private Future<CQLWrapper> buildSqlWrapper(String query, int offset, int limit) {
     try {
-      PostgresClient postgresClient = PostgresClient.getInstance(vertx, tenantId);
-
-      logger.info("CQL Query: " + query);
-
-      String[] fieldList = {"*"};
-
       CQL2PgJSON cql2pgJson = new CQL2PgJSON(EVENT_CONFIGS + ".jsonb");
-      CQLWrapper cql = new CQLWrapper(cql2pgJson, query)
+      CQLWrapper cqlWrapper = new CQLWrapper(cql2pgJson, query)
         .setLimit(new Limit(limit))
         .setOffset(new Offset(offset));
-
-      postgresClient.get(EVENT_CONFIGS, EventEntity.class, fieldList, cql,
-        true, false, reply -> {
-          try {
-            if (reply.succeeded()) {
-              List<EventEntity> eventEntities = reply.result().getResults();
-
-              EventConfigCollection configCollection =
-                new EventConfigCollection()
-                  .withEventEntity(eventEntities)
-                  .withTotalRecords(reply.result().getResultInfo().getTotalRecords());
-              asyncResultHandler.handle(succeededFuture(
-                GetEventConfigResponse.respond200WithApplicationJson(configCollection)));
-            } else {
-              asyncResultHandler.handle(succeededFuture(
-                GetEventConfigResponse.respond500WithTextPlain(reply.cause().getMessage())));
-            }
-          } catch (Exception e) {
-            logger.error(e);
-            asyncResultHandler.handle(succeededFuture(
-              GetEventConfigResponse.respond500WithTextPlain(e.getMessage())));
-          }
-        });
-    } catch (Exception e) {
-      logger.error(e);
-      asyncResultHandler.handle(succeededFuture(
-        GetEventConfigResponse.respond500WithTextPlain(e.getMessage())));
+      return Future.succeededFuture(cqlWrapper);
+    } catch (FieldException e) {
+      return Future.failedFuture(e);
     }
+  }
+
+  private Future<Results<EventEntity>> getEventConfigCollection(CQLWrapper cqlWrapper, Vertx vertx, String tenantId) {
+    PostgresClient postgresClient = PostgresClient.getInstance(vertx, tenantId);
+
+    String[] fieldList = {"*"};
+    Future<Results<EventEntity>> getFuture = Future.future();
+    postgresClient.get(EVENT_CONFIGS, EventEntity.class, fieldList, cqlWrapper,
+      true, false, getFuture.completer());
+    return getFuture;
+  }
+
+  private EventConfigCollection mapResultsToConfigCollections(Results<EventEntity> results) {
+    return new EventConfigCollection()
+      .withEventEntity(results.getResults())
+      .withTotalRecords(results.getResultInfo().getTotalRecords());
+  }
+
+  private Response mapException(Throwable throwable) {
+    logger.error(throwable);
+    return Response.status(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode())
+      .type(MediaType.TEXT_PLAIN)
+      .entity(Response.Status.INTERNAL_SERVER_ERROR.getReasonPhrase())
+      .build();
   }
 
   @Override
